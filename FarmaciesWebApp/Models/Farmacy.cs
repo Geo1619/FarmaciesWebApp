c34d43740b05caa9056402072bd72e236e05a880
Giorgos <gi0rg0sdimitr1988@gmail.com>
2020-11-10 16:41:07 +0200

    Created the Model
    Added Farmacy and Location to the Model and migrated to SQL DB

---
+++ FarmaciesWebApp/Models/Farmacy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Razor.Text;
+
+namespace FarmaciesWebApp.Models
+{
+    public class Farmacy
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(maximumLength:255,MinimumLength =2,ErrorMessage ="Please type a valid Name")]
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public short PostalCode { get; set; }
+        [Required]
+        public Location Location { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
--- FarmaciesWebApp/Persistence/ApplicationDbContext.cs
+++ FarmaciesWebApp/Persistence/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,9 @@ namespace FarmaciesWebApp.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Farmacy> Farmacies { get; set; }
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("FarmaciesContext", throwIfV1Schema: false)
         {
         }
 
